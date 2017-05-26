66e3301ed4d20d754889cf32953bf4bd5a8b3686
wang_wonderful <wang_wonderful@foxmail.com>
2017-05-26 14:49:46 +0800

    add

---
+++ UI/UILabel+Utility.h
@@ -0,0 +1,15 @@
+//
+//  UILabel+Utility.h
+//  DycApp
+//
+//  Created by HaKim on 2017/5/26.
+//  Copyright © 2017年 haKim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (Utility)
+
++ (NSArray *)getLinesArrayOfStringInLabel:(UILabel *)label;
+
+@end
---
+++ UI/UILabel+Utility.m
@@ -0,0 +1,45 @@
+//
+//  UILabel+Utility.m
+//  DycApp
+//
+//  Created by HaKim on 2017/5/26.
+//  Copyright © 2017年 haKim. All rights reserved.
+//
+
+#import "UILabel+Utility.h"
+#import <CoreText/CoreText.h>
+
+@implementation UILabel (Utility)
+
++ (NSArray *)getLinesArrayOfStringInLabel:(UILabel *)label{
+    NSString *text = [label text];
+    UIFont *font = [label font];
+    CGRect rect = [label bounds];
+    
+    CTFontRef myFont = CTFontCreateWithName(( CFStringRef)([font fontName]), [font pointSize], NULL);
+    NSMutableAttributedString *attStr = [[NSMutableAttributedString alloc] initWithString:text];
+    [attStr addAttribute:(NSString *)kCTFontAttributeName value:(__bridge  id)myFont range:NSMakeRange(0, attStr.length)];
+    CFRelease(myFont);
+    CTFramesetterRef frameSetter = CTFramesetterCreateWithAttributedString(( CFAttributedStringRef)attStr);
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathAddRect(path, NULL, CGRectMake(0,0,rect.size.width,100000));
+    CTFrameRef frame = CTFramesetterCreateFrame(frameSetter, CFRangeMake(0, 0), path, NULL);
+    NSArray *lines = ( NSArray *)CTFrameGetLines(frame);
+    NSMutableArray *linesArray = [[NSMutableArray alloc]init];
+    for (id line in lines) {
+        CTLineRef lineRef = (__bridge  CTLineRef )line;
+        CFRange lineRange = CTLineGetStringRange(lineRef);
+        NSRange range = NSMakeRange(lineRange.location, lineRange.length);
+        NSString *lineString = [text substringWithRange:range];
+        CFAttributedStringSetAttribute((CFMutableAttributedStringRef)attStr, lineRange, kCTKernAttributeName, (CFTypeRef)([NSNumber numberWithFloat:0.0]));
+        CFAttributedStringSetAttribute((CFMutableAttributedStringRef)attStr, lineRange, kCTKernAttributeName, (CFTypeRef)([NSNumber numberWithInt:0.0]));
+        [linesArray addObject:lineString];
+    }
+    
+    CGPathRelease(path);
+    CFRelease( frame );
+    CFRelease(frameSetter);
+    return (NSArray *)linesArray;
+}
+
+@end
